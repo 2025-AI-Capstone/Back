b76128e2a1ce07a60488b47b5c736f255984a28f
suwdle <suwdle1917@gmail.com>
2025-06-09 15:48:09 +0900

    UPDATE system.py

--- routers/system.py
+++ routers/system.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 import models, schemas
 from database import get_db
+from sqlalchemy import desc
+from sqlalchemy.orm import aliased
 
 router = APIRouter()
 
@@ -18,8 +20,24 @@ def create_system_status(
     return new_system
 
 # ────────────── 전체 시스템 상태 조회 ──────────────
+
 @router.get("/system-statuses", response_model=list[schemas.SystemStatusResponse])
 def get_system_statuses(
     db: Session = Depends(get_db)
 ):
-    return db.query(models.SystemStatus).all()
+    subq = (
+        db.query(
+            models.SystemStatus.node_name,
+            db.func.max(models.SystemStatus.id).label("max_id")
+        )
+        .group_by(models.SystemStatus.node_name)
+        .subquery()
+    )
+
+    latest_statuses = (
+        db.query(models.SystemStatus)
+        .join(subq, models.SystemStatus.id == subq.c.max_id)
+        .all()
+    )
+
+    return latest_statuses
